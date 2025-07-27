2d7d37a0d279f693fbf83ed18d22881841c5fc3e
nearni <79463294+nearni@users.noreply.github.com>
2025-07-27 13:58:22 +0800

    Update products.js

--- js/products.js
+++ js/products.js
@@ -67,24 +67,24 @@ document.addEventListener("DOMContentLoaded", async () => {
 });
 
 function addToCart1(id) {
-  const cart = JSON.parse(localStorage.getItem("cart")) || [];
+  const cart = JSON.parse(localStorage.getItem("cart_B")) || [];
   const existing = cart.find(item => item.id === id);
   if (existing) {
     existing.quantity += 1;
   } else {
     cart.push({ id, quantity: 1 });
   }
-  localStorage.setItem("cart", JSON.stringify(cart));
+  localStorage.setItem("cart_B", JSON.stringify(cart));
   alert("已加入購物車！");
 }
 function addToCart(product) {
-  const cart = JSON.parse(localStorage.getItem("cart")) || [];
+  const cart = JSON.parse(localStorage.getItem("cart_B")) || [];
   const existing = cart.find(item => item.id === product.id);
   if (existing) {
     existing.quantity += 1;
   } else {
     cart.push({ ...product, quantity: 1 });
   }
-  localStorage.setItem("cart", JSON.stringify(cart));
+  localStorage.setItem("cart_B", JSON.stringify(cart));
   alert("已加入購物車！");
 }
