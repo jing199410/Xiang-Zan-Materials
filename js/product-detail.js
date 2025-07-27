c8ad65a803d1dd02167af9e5b9d399f103e2e8de
nearni <79463294+nearni@users.noreply.github.com>
2025-07-28 00:22:32 +0800

    Update product-detail.js

--- js/product-detail.js
+++ js/product-detail.js
@@ -10,10 +10,10 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   try {
     const res = await fetch('./products.json');
-    const text = await res.text();
+    //const text = await res.text();
     //console.log(text);  // 檢查拿到的資料是不是 JSON
-    
-    const products = JSON.parse(text);
+    const products = await res.json();
+    //const products = JSON.parse(text);
     const product = products.find(p => p.id === productId);
 
     if (!product) {
